54f1accd01abd42dd49c7a8b4c6597ff701762e0
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2023-01-22 14:41:36 -0600

    can return json for e2e

---
+++ amulet_backend/api/amulet_model/__tests__/test_mana.py
@@ -0,0 +1,53 @@
+import pytest
+
+from ..mana import mana, Mana
+
+
+def test_wrapper():
+    assert mana("2GG") == Mana(green=2, total=4)
+    assert mana("") == Mana(green=0, total=0)
+    assert mana("WUBRG") == Mana(green=1, total=5)
+    assert mana("G123G") == Mana(green=2, total=8)
+
+
+def test_ge():
+    assert mana("3GG") >= mana("1GG")
+    assert mana("3GG") >= mana("3GG")
+    # No comparison
+    assert not mana("8G") >= mana("GG")
+    assert not mana("GG") >= mana("8G")
+
+
+def test_le():
+    assert mana("1GG") <= mana("3GG")
+    assert mana("3GG") <= mana("3GG")
+    # No comparison
+    assert not mana("GG") <= mana("8G")
+    assert not mana("8G") <= mana("GG")
+
+
+def test_eq():
+    assert mana("3GG") == mana("3GG")
+
+
+def test_add():
+    assert mana("2G") + mana("1G") == mana("3GG")
+
+
+def test_sub():
+    assert mana("2G") - mana("1G") == mana("1")
+    assert mana("2GG") - mana("3") == mana("G")
+
+
+def test_mul():
+    assert mana("1G") * 3 == mana("3GGG")
+    assert mana("1G") * 0 == mana("0")
+
+
+def test_bool():
+    assert bool(mana("")) is False
+    assert bool(mana("1")) is True
+
+
+if __name__ == "__main__":
+    pytest.main()
--- amulet_backend/api/views.py
+++ amulet_backend/api/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from typing import List
+from django.http import JsonResponse
+
+from .amulet_model import GameManager
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    deck_list = load_deck_list()
+    notes = GameManager.run_e2e(deck_list)
+
+    return JsonResponse({"notes": notes})
+
+
+def load_deck_list() -> List[str]:
+    deck_list = []
+    with open("assets/deck-list.txt") as handle:
+        for line in handle:
+            if line.startswith("#") or not line.strip():
+                continue
+            n, card_name = line.rstrip().split(None, 1)
+            deck_list += [card_name] * int(n)
+    return deck_list
