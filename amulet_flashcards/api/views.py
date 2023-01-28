d1bfa4770709f504df5b135245834faf2a31d439
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2023-01-27 19:09:53 -0600

    tidy up types

--- amulet_flashcards/api/views.py
+++ amulet_flashcards/api/views.py
@@ -4,14 +4,14 @@ from django.http import HttpRequest, HttpResponse
 from .amulet_model import GameManager, HtmxHelper
 
 
-def e2e(request):
+def e2e(request: HttpRequest) -> HttpResponse:
     deck_list = load_deck_list()
     opener = GameManager.get_opener_from_deck_list(deck_list)
     summary = GameManager.run_from_opener(opener)
     return HttpResponse(HtmxHelper.from_play_summary(summary))
 
 
-def opener(request):
+def opener(request: HttpRequest) -> HttpResponse:
     deck_list = load_deck_list()
     opener = GameManager.get_opener_from_deck_list(deck_list)
     return HttpResponse(HtmxHelper.from_opener(opener))
