60a8821eb7a77a4b439b9d9f0022cf0bc3cb785e
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2023-02-01 16:09:04 -0600

    js tidying

--- amulet_flashcards/frontend/static/js/app.js
+++ amulet_flashcards/frontend/static/js/app.js
@@ -1,3 +1,16 @@
+/* Disable the buttons while working on a request */
+
+htmx.on('htmx:beforeRequest', function(evt) {
+    document.getElementById("opener-button").toggleAttribute("disabled");
+    document.getElementById("play-button").toggleAttribute("disabled");
+});
+
+
+htmx.on('htmx:afterRequest', function(evt) {
+    document.getElementById("opener-button").removeAttribute("disabled");
+    document.getElementById("play-button").removeAttribute("disabled");
+});
+
 /* Display card image when a card name is clicked. Next click hides it. */
 
 function show_autocard(card_image_url) {
@@ -16,29 +29,19 @@ function show_about() {
     document.getElementById("about").style.display = "block";
 }
 
-document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    if (evt.key == "Escape" || evt.key == "Esc") {
-        hide_about()
-    }
-};
-
 function hide_about() {
     document.getElementById("main").style.display = "block";
     document.getElementById("about").style.display = "none";
 }
 
-/* Disable the buttons while working on a request */
+/* If the user is mashing buttons, close everything */
 
-htmx.on('htmx:beforeRequest', function(evt) {
-    document.getElementById("opener-button").toggleAttribute("disabled");
-    document.getElementById("play-button").toggleAttribute("disabled");
-});
-
-
-htmx.on('htmx:afterRequest', function(evt) {
-    document.getElementById("opener-button").removeAttribute("disabled");
-    document.getElementById("play-button").removeAttribute("disabled");
-});
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.key == "Escape" || evt.key == "Esc") {
+        hide_about();
+        hide_autocard();
+    }
+};
 
 
