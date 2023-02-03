8af85a1d88a5c51ad834cfba713ad891cdcf7997
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2023-02-03 12:58:45 -0600

    gunicorn works locally, not yet in docker

--- amulet_flashcards/amulet_flashcards/wsgi.py
+++ amulet_flashcards/amulet_flashcards/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amulet_flashcards.settings")
 
-application = get_wsgi_application()
+
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
